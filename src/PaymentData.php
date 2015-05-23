a56c022226d898a0be248e2f87b9cd97c0fd6c5b
Remco Tolsma <info@remcotolsma.nl>
2015-05-23 11:36:35 +0200

    Updated code block copyright year to 2015 and version to 1.0.0.

--- src/PaymentData.php
+++ src/PaymentData.php
@@ -3,10 +3,10 @@
 /**
  * Title: ClassiPress iDEAL data proxy
  * Description:
- * Copyright: Copyright (c) 2005 - 2011
+ * Copyright: Copyright (c) 2005 - 2015
  * Company: Pronamic
  * @author Remco Tolsma
- * @version 1.0
+ * @version 1.0.0
  */
 class Pronamic_WP_Pay_Extensions_ClassiPress_PaymentData extends Pronamic_WP_Pay_PaymentData {
 	/**
