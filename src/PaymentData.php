a7605786732aa427fae4d7473457c16423075bd2
Remco Tolsma <info@remcotolsma.nl>
2016-02-12 09:27:54 +0100

    Happy 2016.

--- src/PaymentData.php
+++ src/PaymentData.php
@@ -3,7 +3,7 @@
 /**
  * Title: ClassiPress iDEAL data proxy
  * Description:
- * Copyright: Copyright (c) 2005 - 2015
+ * Copyright: Copyright (c) 2005 - 2016
  * Company: Pronamic
  * @author Remco Tolsma
  * @version 1.0.0
