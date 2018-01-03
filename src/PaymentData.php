4cd814be686cd15b17000bae5e5c2da01073fc29
Reüel van der Steege <reuel@pronamic.nl>
2018-01-03 10:53:15 +0100

    Happy 2018.

--- src/PaymentData.php
+++ src/PaymentData.php
@@ -3,7 +3,7 @@
 /**
  * Title: ClassiPress iDEAL data proxy
  * Description:
- * Copyright: Copyright (c) 2005 - 2017
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
