de66cae3ed09d7d70af74728d7606892ece683ab
Reüel van der Steege <reuel@pronamic.nl>
2020-01-02 14:32:05 +0100

    Happy 2020.

--- src/PaymentData.php
+++ src/PaymentData.php
@@ -9,7 +9,7 @@ use Pronamic\WordPress\Pay\Payments\Items;
 /**
  * Title: ClassiPress iDEAL data proxy
  * Description:
- * Copyright: 2005-2019 Pronamic
+ * Copyright: 2005-2020 Pronamic
  * Company: Pronamic
  *
  * @author  Remco Tolsma
